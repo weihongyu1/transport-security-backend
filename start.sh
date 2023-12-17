2f9d37479f1e534ad999972570ff22a1e9a92a71
weihongyu1 <488009667@qq.com>
2023-12-17 16:14:31 +0800

    修改启动脚本

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 mvn clean package
-docker build -t transport-security:v2.0 .
-docker run -p 8888:8888 --name transport-security-container --link mysql:mysql-container --link redis:redis-container -d transport-security:v2.0
+docker build -t transport-security-backend:v2.0 .
+docker run -p 8888:8888 --name transport-security-backend-container --link mysql:mysql-container --link redis:redis-container -d transport-security-backend:v2.0
